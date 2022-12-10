f2a2300f783df26574a4d41c549294275dddd24f
superiorkid <bayipetani@protonmail.com>
2022-12-10 09:39:01 +0800

    initial commit

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var app = express();
 // so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.set('trust proxy', true)
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -23,6 +24,16 @@ app.get('/', function (req, res) {
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/whoami', (req, res) => {
+  const output = {
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  }
+  
+  res.json(output)
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
